2448e569216a07a514aeb1a8cab252506717ce01
Jitin20 <jitinsharma2001@rediffmail.com>
2022-08-12 14:30:04 +0530

    Commit

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -10,7 +10,7 @@ namespace HelloApp
             Console.WriteLine("Welcome to Jenkins");
             Calc calc = new Calc();
             Console.WriteLine($"Sum = {calc.Add(70,80)}");
-            Console.WriteLine($"Multiply = {calc.Multiply(4, 5)}");
+            Console.WriteLine($"Multiply = {calc.Multiply(6, 5)}");
             Console.ReadLine();
         }
     }
